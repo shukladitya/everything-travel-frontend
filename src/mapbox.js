78d86eae430b7a430006242986a5b9519c9fbe29
shukladitya <shukladitya9@gmail.com>
2023-07-01 18:14:46 +0530

    globe styling

--- src/mapbox.js
+++ src/mapbox.js
@@ -13,8 +13,10 @@ const MapboxMap = () => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: "mapbox://styles/mapbox/dark-v10",
-      center: [0, 0],
-      zoom: 1,
+      center: [795.956, 21.9162],
+      zoom: 4.5,
+      minZoom: 2.3,
+      projection: "globe",
     });
 
     const geocoder = new MapboxGeocoder({
@@ -24,6 +26,80 @@ const MapboxMap = () => {
 
     map.addControl(geocoder);
 
+    map.on("load", () => {
+      map.setFog({
+        range: [-1, 2],
+        "horizon-blend": 0.3,
+        color: "#242B4B",
+        "high-color": "#161B36",
+        "space-color": "#0B1026",
+        "star-intensity": 1,
+      });
+    });
+
+    //
+    //
+    //globe rotation
+
+    // At low zooms, complete a revolution every two minutes.
+    const secondsPerRevolution = 120;
+    // Above zoom level 5, do not rotate.
+    const maxSpinZoom = 5;
+    // Rotate at intermediate speeds between zoom levels 3 and 5.
+    const slowSpinZoom = 3;
+
+    let userInteracting = false;
+    let spinEnabled = true;
+
+    function spinGlobe() {
+      const zoom = map.getZoom();
+      if (spinEnabled && !userInteracting && zoom < maxSpinZoom) {
+        let distancePerSecond = 360 / secondsPerRevolution;
+        if (zoom > slowSpinZoom) {
+          // Slow spinning at higher zooms
+          const zoomDif = (maxSpinZoom - zoom) / (maxSpinZoom - slowSpinZoom);
+          distancePerSecond *= zoomDif;
+        }
+        const center = map.getCenter();
+        center.lng -= distancePerSecond;
+        // Smoothly animate the map over one second.
+        // When this animation is complete, it calls a 'moveend' event.
+        map.easeTo({ center, duration: 1000, easing: (n) => n });
+      }
+    }
+
+    // Pause spinning on interaction
+    map.on("mousedown", () => {
+      userInteracting = true;
+    });
+
+    // Restart spinning the globe when interaction is complete
+    map.on("mouseup", () => {
+      userInteracting = false;
+      spinGlobe();
+    });
+
+    // These events account for cases where the mouse has moved
+    // off the map, so 'mouseup' will not be fired.
+    map.on("dragend", () => {
+      userInteracting = false;
+      spinGlobe();
+    });
+    map.on("pitchend", () => {
+      userInteracting = false;
+      spinGlobe();
+    });
+    map.on("rotateend", () => {
+      userInteracting = false;
+      spinGlobe();
+    });
+
+    // When animation is complete, start spinning if there is no ongoing interaction
+    map.on("moveend", () => {
+      spinGlobe();
+    });
+
+    spinGlobe();
     // Clean up on component unmount
     return () => map.remove();
   }, []);
